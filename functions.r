2aacc628a3f8dc49ea436f42c4577ae1d02ea794
qianwang1013 <qianwang1013@hotmail.com>
2015-10-10 20:56:25 -0400

    Ripper added

--- functions.r
+++ functions.r
@@ -1,5 +1,5 @@
 ##############################################
-############### IRIS
+############### IRIS User Interact Functions
 
 iris_myC45 <- function(){
   library(datasets)
@@ -8,16 +8,23 @@ iris_myC45 <- function(){
 
 }
 
-iris_myC45Predict <- function(){
-  fit <- iris_myC45()
+iris_myC45Predict<-function(){
+  iris_Predict(iris_myC45())
+}
+
+iris_Ripper <- function(){
   library(datasets)
   data("iris")
-  myC45Predict(fit, iris[,1:4], iris$Species)
-
+  myRipper(Species~., iris)
+}
+iris_RipperPredict <- function(){
+  ####Calling middile group function passing correct fit
+  iris_Predict(iris_Ripper())
 }
 
 ##############################################
-###############Life Expectancy
+############### Life Expectancy User Interact Functions
+
 
 LE_myC45 <- function(){
   LE <- getLE()
@@ -26,8 +33,55 @@ LE_myC45 <- function(){
 
 }
 
-LE_myC45Predict <- function(){
-  fit <- LE_myC45()
+LE_myC45Predict <-function(){
+  ####Calling middile group function passing correct fit
+
+  LE_Predict(LE_myC45())
+}
+
+LE_RipperPredict <- function(){
+  ####Calling middile group function passing correct fit
+
+  LE_Predict(LE_Ripper())
+}
+
+LE_Ripper <- function(){
+  LE <- getLE()
+  myRipper(Continent~., LE)
+}
+
+
+############################################
+#### Base Methods #########################
+##########################################
+
+
+
+##### C45 and its prediction
+myC45 <- function(subset, dataset){
+  #install.packages("RWeka", repos="https://cran.r-project.org/")
+  library(RWeka)
+  J48(subset, dataset)
+}
+iris_Predict <- function(fit){
+  library(datasets)
+  data("iris")
+  myC45Predict(fit, iris[,1:4], iris$Species)
+}
+
+myC45Predict <- function(fit, sample, Sample){
+  library(RWeka)
+  predictions <- predict(fit, sample)
+  table(predictions, Sample)
+}
+
+##### Ripper and its prediction
+myRipper <- function(data, class){
+  library(RWeka)
+  JRip(data, class);
+
+}
+LE_Predict <- function(fit){
   LE <- getLE()
   myC45Predict(fit, LE[,1:7], LE$Continent)
 
@@ -38,19 +92,19 @@ getLE <- function(){
   LE
 }
 
-############################################
-myC45 <- function(subset, dataset){
-  #install.packages("RWeka", repos="https://cran.r-project.org/")
-  library(RWeka)
-  fit <- J48(subset, dataset)
-}
 
-myC45Predict <- function(fit, sample, Sample){
-  library(RWeka)
-  predictions <- predict(fit, sample)
-  table(predictions, Sample)
-}
+
+
+
 ##########################################
+#########################################
+######### Ripper ########################
+
+
+
+
+
+#####################################
 
 
 #####################################
@@ -66,3 +120,4 @@ divideDataset <- function(ori_set){
   dataset <- list( training  = training,test = test)
   dataset
 }
+
